454868c09b663965638a4c65c19295d78ed2fdc6
alexsavinov <spell477@gmail.com>
2023-04-20 10:24:04 +0300

    Initial commit

---
+++ src/main/java/com/epam/esm/epammodule3/EpamModule3Application.java
@@ -0,0 +1,13 @@
+package com.epam.esm.epammodule3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpamModule3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpamModule3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/esm/epammodule3/EpamModule3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.esm.epammodule3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpamModule3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
